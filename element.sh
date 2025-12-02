39e618a4009704e7ad45fdb89be69e13538d812f
jekinney42 <jekinney@mail.fhsu.edu>
2025-12-02 04:11:30 +0000

    feat: add element.sh script

---
+++ element.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+  exit
+fi
+
+# Check if input is a number (atomic_number)
+if [[ $1 =~ ^[0-9]+$ ]]
+then
+  ELEMENT=$($PSQL "SELECT e.atomic_number, e.symbol, e.name, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius FROM elements e JOIN properties p ON e.atomic_number = p.atomic_number JOIN types t ON p.type_id = t.type_id WHERE e.atomic_number = $1")
+else
+  # Input is a symbol or name
+  ELEMENT=$($PSQL "SELECT e.atomic_number, e.symbol, e.name, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius FROM elements e JOIN properties p ON e.atomic_number = p.atomic_number JOIN types t ON p.type_id = t.type_id WHERE e.symbol = '$1' OR e.name = '$1'")
+fi
+
+if [[ -z $ELEMENT ]]
+then
+  echo "I could not find that element in the database."
+else
+  echo "$ELEMENT" | while IFS="|" read ATOMIC_NUMBER SYMBOL NAME TYPE MASS MELTING BOILING
+  do
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
+  done
